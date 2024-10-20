dde8d0256d41ebc763d00999f5e84b19224c54e1
Dots <dotvnl@gmail.com>
2024-10-20 21:00:59 +0300

    basic functionality, add check for command

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
 
+const child = require("child_process");
+const wait = require("timers/promises").setTimeout;
+
 const express = require("express");
 const app = express();
 app.use(express.json());
@@ -8,23 +11,58 @@ const key = process.env.KEY;
 const port = process.env.PORT || 5000;
 
 if (!key) {
-  return console.error("Missing key");
+  return console.error("Missing key from .env");
 }
 
 app.post("/command", (req, res) => {
   const auth = req.headers["authorization"];
   if (auth !== key) {
+    console.warn("Unauthorized request");
     return res.status(401).send("Unauthorized");
   }
 
-  const { command } = req.body;
+  const command = req.body.command;
+  if (!command) {
+    console.warn("Authorized request, but Missing command");
+    return res.status(400).send("Missing command");
+  }
   console.log(`Received command: ${command}`);
 
-  // TODO: The actual CMDs
+  const process = child.spawn(command, {
+    shell: true,
+    stdio: "inherit",
+  });
 
-  res.status(200).send(`Command received: ${command}`);
+  let statusErr = "";
+  let statusOut = "";
+
+  process.stdout.on("data", (data) => {
+    statusOut += data;
+  });
+
+  process.stderr.on("data", (data) => {
+    statusErr += data;
+  });
+
+  process.on("error", (error) => {
+    console.error(`Command error: ${error.message}`);
+  });
+
+  process.on("close", (code) => {
+    console.log(`Command exited with code ${code}`);
+    const response = {
+      stdout: statusOut,
+      stderr: statusErr,
+      exitCode: code,
+    };
+
+    if (statusOut.includes("OK")) {
+      return res.status(200).send(response);
+    } else if (statusOut.includes("FAIL")) {
+      return res.status(500).send(response);
+    }
+  });
 });
-
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
